75474d9af3b93cc01b04df1726c274797f0b14cd
vettaelizaveta <113547543+vettaelizaveta@users.noreply.github.com>
2022-11-28 23:22:13 +0500

    ДЗ 6

--- src/js/calc.js
+++ src/js/calc.js
@@ -1,37 +1,76 @@
-"use strict"
-
-
-
-if (confirm("Хотите ли вы расчитать сумму вашего сайта?")){
-let arr = [];
-function siteType(){
-    arr[0]=prompt("Выберите тип сайта и укажите в строку его цену (только число): \n 1) Первый тип - 1000р \n 2)Второй тип - 2000р \n 3)Третий тип - 3000р");
-}
-    
-function design(){
-    arr[1]=prompt("Выберите дизайн сайта и укажите в строку его цену (только число): \n 1) Первый дизайн - 1000р \n 2)Второй дизайн - 2000р \n 3)Третий дизайн - 3000р");
-}
-    
-function adapt(){
-    arr[2]=prompt("Выберите адаптивность сайта и укажите в строку его цену (только число): \n 1) Первая адаптивность - 1000р \n 2)Вторая адаптивность - 2000р \n 3)Третья адаптивность - 3000р");
-}
-
-function orderPrice(){
-    let sum = 0, day=0;
-    sum=Number(sum);
-       day=Number(day);
-    for (let i=0; i<3;i++){
-        arr[i]=Number(arr[i]);
-        sum+=arr[i];
-        day=day+arr[i]/1000;
+$(document).ready(function(){
+    $(".loader").css("display","none");
+     function calculate (){
+         let sum = parseInt($("#price1 option:selected").val())+ parseInt($("#price2 option:selected").val())+ parseInt($("#price3 option:selected").val());
+   let days = sum/1000;
+    $(".days .total").text(days);
+        $(".price .total").text(sum);
     }
-    alert("Сумма вашего сайта: "+sum+"\n Время выполнения: "+day+" дня");
-}
+$("select").on("change", function(){
+ calculate();
+});
+   calculate(); 
     
- siteType();
- design();
-adapt();
-orderPrice();
-}
+    
+    
+    
+    
+    var isInViewport = function (elem) {
+            var distance = elem.getBoundingClientRect();
+            return (
+                distance.top >= 0 &&
+                distance.left >= 0 &&
+                distance.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+                distance.right <= (window.innerWidth || document.documentElement.clientWidth)
+            );
+        };
+        
+        var findMe = document.querySelector('.counter-start');
+
+        window.addEventListener('scroll', function (event) {
+            if (isInViewport(findMe)) {
+                setTimeout(function () {
+
+            odometer1.innerHTML = 120;
+            odometer2.innerHTML = 460;
+          odometer3.innerHTML = 340;
+          odometer4.innerHTML = 23;
+        }, 100);
+    
+            }
+        }, false);
+    
+   
+    
+    
+    
+    
 
 
+$(window).scroll(() => {
+    let scrollDistnase = $(window).scrollTop();
+    $('.section').each((i,el) => {
+        if ($(el).offset().top - $('nav').onterHeight()<= scrollDistnase){
+            $('nav a').each((e,el) => {
+                if ($(el).hasClass ('active')){
+                    $(el).removeClass ('active');}})
+            
+            $('nav li eq(' + i + ')').find('a').addClass('active');}
+            
+            }); });
+
+    
+    let options = {threshold:[0.5]};
+    let observer = new IntersectionObserver(onEntry, options);
+    let elements = $('.element-animation');
+    elements.each((i,el) => {
+        observer.observe(el); });
+});
+
+
+       function onEntry (entry){
+        entry.forEach (change => {
+            if (change.isIntersecting){
+                change.target.classList.add('show-animation');
+            }} );}              
+                    
